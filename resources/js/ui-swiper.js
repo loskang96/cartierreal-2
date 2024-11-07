657ecf4c7f513571f7b13247a709684e2ae4f1dd
loskang96 <gksthf9561@gmail.com>
2024-11-07 15:08:20 +0900

    [cartier]사진비교

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -13,3 +13,23 @@ var swiper = new Swiper('.swiper-partners', {
         disableOnInteraction: false,
     },
 });
+
+gsap.fromTo(
+    '.intro',
+    {
+        y: 0, // 시작 위치
+        opacity: 1, // 시작 투명도
+    },
+    {
+        y: -100, // 스크롤 시 위로 이동할 거리
+        opacity: 0, // 투명하게 사라짐
+        scrollTrigger: {
+            trigger: '.intro', // 애니메이션이 적용될 요소
+            start: 'top 80%', // 트리거 시작 지점 (화면의 80% 지점에 도달할 때 시작)
+            end: 'top 30%', // 트리거 종료 지점 (화면의 30% 지점에 도달할 때 끝)
+            scrub: true, // 스크롤에 따라 애니메이션이 자연스럽게 진행
+            markers: true, // 디버깅용 마커 표시
+        },
+        duration: 1.5, // 애니메이션 지속 시간
+    }
+);
