6fb15235f9a2ce8e8ca9e8898ac946bf1136ab68
loskang96 <gksthf9561@gmail.com>
2024-11-20 18:20:28 +0900

    [cartier]feat.gsap추가로 인한 레이아웃 변경1

--- resources/js/ui-swiper.js
+++ resources/js/ui-swiper.js
@@ -14,120 +14,133 @@ var swiper = new Swiper('.swiper-partners', {
     },
 });
 
-gsap.matchMedia().add('(min-width: 1024px)', () => {
-    gsap.fromTo(
-        '.intro',
-        {
-            y: 0, // 시작 위치
-            opacity: 1, // 시작 투명도
-        },
-        {
-            y: -100, // 스크롤 시 위로 이동할 거리
-            opacity: 0, // 투명하게 사라짐
-            scrollTrigger: {
-                trigger: '.intro', // 애니메이션이 적용될 요소
-                start: 'top 40%', // 트리거 시작 지점 (화면의 80% 지점에 도달할 때 시작)
-                end: 'top 2%', // 트리거 종료 지점 (화면의 30% 지점에 도달할 때 끝)
-                scrub: true, // 스크롤에 따라 애니메이션이 자연스럽게 진행
-                markers: false, // 디버깅용 마커 표시
-                // duration: 1, // 애니메이션 지속 시간
-            },
-        }
-    );
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.defaults({
+    invalidateOnRefresh: true,
 });
+
+gsap.fromTo(
+    '.intro',
+    {
+        y: 0, // 시작 위치
+        opacity: 1, // 시작 투명도
+    },
+    {
+        y: -100, // 스크롤 시 위로 이동할 거리
+        opacity: 0, // 투명하게 사라짐
+        scrollTrigger: {
+            trigger: '.intro', // 애니메이션이 적용될 요소
+            start: 'top 15%', // 트리거 시작 지점
+            end: 'top 1%', // 트리거 종료 지점
+            scrub: true, // 스크롤에 따라 자연스럽게 진행
+            markers: false, // 디버깅용 마커 표시
+        },
+    }
+);
+
 gsap.matchMedia().add(
     '(min-width: 1024px)',
     () => {
         gsap.to('.sec-cartier .sec-g__img', {
-            yPercent: -100, // y축으로 50% 이동
+            y: -100, // y축으로 50% 이동
             ease: 'none',
             scrollTrigger: {
                 trigger: '.sec-cartier .sec-g__img', // 트리거 요소 설정
                 start: 'top bottom', // 시작 위치
                 end: 'bottom top', // 끝 위치
                 scrub: 1, // 스크롤에 따라 자연스럽게 이동
+                // markers: true,
             },
         });
     },
     gsap.to('.sec-cartier .sec-g__tit', {
-        yPercent: -390, // y축으로 50% 이동
+        y: -390, // y축으로 50% 이동
         ease: 'none',
         scrollTrigger: {
             trigger: '.sec-cartier .sec-g__tit', // 트리거 요소 설정
             start: 'top bottom', // 시작 위치
             end: 'bottom top', // 끝 위치
             scrub: 1, // 스크롤에 따라 자연스럽게 이동
+            // markers: true,
         },
     })
 );
 
 gsap.to('.sec-cartier .sec-g__in', {
-    yPercent: -400, // y축으로 50% 이동
+    y: -400, // y축으로 50% 이동
     ease: 'none',
     scrollTrigger: {
         trigger: '.sec-cartier .sec-g__in', // 트리거 요소 설정
         start: 'top bottom', // 시작 위치
         end: 'bottom top', // 끝 위치
         scrub: 1, // 스크롤에 따라 자연스럽게 이동
+        // markers: true,
     },
 });
 gsap.to('.sec-cartier2 .sec-g__tit', {
-    yPercent: -600, // y축으로 50% 이동
+    y: -600, // y축으로 50% 이동
     ease: 'none',
     scrollTrigger: {
         trigger: '.sec-cartier2 .sec-g__tit', // 트리거 요소 설정
         start: 'top bottom', // 시작 위치
         end: 'bottom top', // 끝 위치
         scrub: 1, // 스크롤에 따라 자연스럽게 이동
+        // markers: true,
     },
 });
 gsap.to('.sec-cartier2 .sec-g__desc', {
-    yPercent: -500, // y축으로 50% 이동
+    y: -500, // y축으로 50% 이동
     ease: 'none',
     scrollTrigger: {
         trigger: '.sec-cartier2 .sec-g__desc', // 트리거 요소 설정
         start: 'top bottom', // 시작 위치
         end: 'bottom top', // 끝 위치
         scrub: 1, // 스크롤에 따라 자연스럽게 이동
+        // markers: true,
     },
 });
 gsap.to('.sec-cartier2 .lst-cartier2', {
-    yPercent: -250, // y축으로 50% 이동
+    y: -250, // y축으로 50% 이동
     ease: 'none',
     scrollTrigger: {
         trigger: '.sec-cartier2 .lst-cartier2', // 트리거 요소 설정
         start: 'top bottom', // 시작 위치
         end: 'bottom top', // 끝 위치
         scrub: 1, // 스크롤에 따라 자연스럽게 이동
+        // markers: true,
     },
 });
 gsap.to('.sec-cartier3 .sec-g__tit', {
-    yPercent: -700, // y축으로 50% 이동
+    y: -700, // y축으로 50% 이동
     ease: 'none',
     scrollTrigger: {
         trigger: '.sec-cartier3 .sec-g__tit', // 트리거 요소 설정
         start: 'top bottom', // 시작 위치
         end: 'bottom top', // 끝 위치
         scrub: 1, // 스크롤에 따라 자연스럽게 이동
+        // markers: true,
     },
 });
 gsap.to('.sec-cartier3 .lst-cartier3', {
-    yPercent: -700, // y축으로 50% 이동
+    y: -700, // y축으로 50% 이동
     ease: 'none',
     scrollTrigger: {
         trigger: '.sec-cartier3 .lst-cartier3', // 트리거 요소 설정
         start: 'top bottom', // 시작 위치
         end: 'bottom top', // 끝 위치
         scrub: 1, // 스크롤에 따라 자연스럽게 이동
+        // markers: true,
     },
 });
 gsap.to('.sec-cartier4 .sec-g__tit', {
-    yPercent: -100, // y축으로 50% 이동
+    y: -100, // y축으로 50% 이동
     ease: 'none',
     scrollTrigger: {
         trigger: '.sec-cartier4 .sec-g__tit', // 트리거 요소 설정
         start: 'top bottom', // 시작 위치
         end: 'bottom top', // 끝 위치
         scrub: 1, // 스크롤에 따라 자연스럽게 이동
+        // markers: true,
     },
 });
